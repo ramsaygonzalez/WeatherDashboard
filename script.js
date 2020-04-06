6a4d22ccaf68ab84b0b6eaad6439b0f78ee92938
ramsayg <ramsaygonzalez@outlook.com>
2020-04-05 20:26:55 -0400

    created ajax call for each item required to display in the html file from openweather api.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var APIKey = "15e6baf0e24eef095c6a896d731ad257";
 var requestedCity = "miami";
 var currentWXURL = "https://api.openweathermap.org/data/2.5/weather?q=" + requestedCity + "&appid=" + APIKey
-var forecastURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + requestedCity + "&cnt=" + 5 + "&appid=" + APIKey
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + requestedCity + "&cnt=" + "&appid=" + APIKey
 var today = new Date();
 
 $.ajax({
@@ -16,6 +16,7 @@ $.ajax({
     console.log("Wind Speed: " + currentWX.wind.speed + " MPH");
     console.log(currentWX.coord.lat);
     console.log(currentWX.coord.lon);
+    console.log(currentWX.weather[0].icon);
 
     var UVIndexURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + currentWX.coord.lat + "&lon=" + currentWX.coord.lon;
 
@@ -25,13 +26,38 @@ $.ajax({
     }).then(function (UVIndex) {
         console.log("UV Index: " + UVIndex.value);
 
-
         $.ajax({
             url: forecastURL,
             method: "GET"
         }).then(function (forecast) {
             console.log(forecast);
+            console.log("Temperature: " + ((forecast.list[4].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
+            console.log("Humidity: " + forecast.list[4].main.humidity + " %");
+            console.log(forecast.list[4].weather[0].icon);
+            console.log("Temperature: " + ((forecast.list[12].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
+            console.log("Humidity: " + forecast.list[12].main.humidity + " %");
+            console.log(forecast.list[12].weather[0].icon);
+            console.log("Temperature: " + ((forecast.list[20].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
+            console.log("Humidity: " + forecast.list[20].main.humidity + " %");
+            console.log(forecast.list[20].weather[0].icon);
+            console.log("Temperature: " + ((forecast.list[28].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
+            console.log("Humidity: " + forecast.list[28].main.humidity + " %");
+            console.log(forecast.list[28].weather[0].icon);
+            console.log("Temperature: " + ((forecast.list[36].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
+            console.log("Humidity: " + forecast.list[36].main.humidity + " %");
+            console.log(forecast.list[36].weather[0].icon);
+
+            // var indexRequired = [4, 12, 20, 28, 36];
+
+            // for (let i = 0; i < forecast.list.length; i++) {
+            //     if (forecast.list[i] = indexRequired) {
+            //         console.log("yes")
+
+            // }
+            //     }
+
         })
+
     })
 })
 
