084541a1c1b68328648dbbe1ca4d58f06af5992d
ramsayg <ramsaygonzalez@outlook.com>
2020-04-06 12:19:23 -0400

    html and js file udpdate to display 5 day forecast information from openweather api.

--- script.js
+++ script.js
@@ -80,35 +80,70 @@ $("#city-search").on("click", function (event) {
                 url: forecastURL,
                 method: "GET"
             }).then(function (forecast) {
+                var dayOneDate = forecast.list[4].dt_txt
                 var dayOneTemp = "Temperature: " + ((forecast.list[4].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F";
                 var dayOneHumid = "Humidity: " + forecast.list[4].main.humidity + " %";
                 var dayOneIcon = forecast.list[4].weather[0].icon;
+                var dayTwoDate = forecast.list[4].dt_txt
                 var dayTwoTemp = "Temperature: " + ((forecast.list[12].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F";
                 var dayTwoHumid = "Humidity: " + forecast.list[12].main.humidity + " %";
                 var dayTwoIcon = forecast.list[12].weather[0].icon;
+                var dayThreeDate = forecast.list[4].dt_txt
                 var dayThreeTemp = "Temperature: " + ((forecast.list[20].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F";
                 var dayThreeHumid = "Humidity: " + forecast.list[20].main.humidity + " %";
                 var dayThreeIcon = forecast.list[20].weather[0].icon;
+                var dayFourDate = forecast.list[4].dt_txt
                 var dayFourTemp = "Temperature: " + ((forecast.list[28].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F";
                 var dayFourHumid = "Humidity: " + forecast.list[28].main.humidity + " %";
                 var dayFourIcon = forecast.list[28].weather[0].icon;
+                var dayFiveDate = forecast.list[4].dt_txt
                 var dayFiveTemp = "Temperature: " + ((forecast.list[36].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F";
                 var dayFiveHumid = "Humidity: " + forecast.list[36].main.humidity + " %";
                 var dayFiveIcon = forecast.list[36].weather[0].icon;
 
+                $("#dayone-date").html("<h5>" + dayOneDate);
+                $("#dayone-date").append($("<img>").attr("src", "http://openweathermap.org/img/w/" + dayOneIcon + ".png"));
+                $("#dayone-temp").text(dayOneTemp);
+                $("#dayone-humid").text(dayOneHumid);
+
+                $("#daytwo-date").html("<h5>" + dayTwoDate);
+                $("#daytwo-date").append($("<img>").attr("src", "http://openweathermap.org/img/w/" + dayTwoIcon + ".png"));
+                $("#daytwo-temp").text(dayTwoTemp);
+                $("#daytwo-humid").text(dayTwoIcon);
+
+                $("#daythree-date").html("<h5>" + dayThreeDate);
+                $("#daythree-date").append($("<img>").attr("src", "http://openweathermap.org/img/w/" + dayThreeIcon + ".png"));
+                $("#daythree-temp").text(dayThreeTemp);
+                $("#daythree-humid").text(dayThreeHumid);
+
+                $("#dayfour-date").html("<h5>" + dayFourDate);
+                $("#dayfour-date").append($("<img>").attr("src", "http://openweathermap.org/img/w/" + dayFourIcon + ".png"));
+                $("#dayfour-temp").text(dayFourTemp);
+                $("#dayfour-temp").text(dayFourHumid);
+
+                $("#dayfive-date").html("<h5>" + dayFiveDate);
+                $("#dayfive-date").append($("<img>").attr("src", "http://openweathermap.org/img/w/" + dayFiveIcon + ".png"));
+                $("#dayfive-temp").text(dayFiveTemp);
+                $("#dayfive-humid").text(dayFiveHumid);
+
                 console.log(forecast)
+                console.log(dayOneDate);
                 console.log(dayOneTemp);
                 console.log(dayOneHumid);
                 console.log(dayOneIcon);
+                console.log(dayTwoDate);
                 console.log(dayTwoTemp);
                 console.log(dayTwoHumid);
                 console.log(dayTwoIcon);
+                console.log(dayThreeDate);
                 console.log(dayThreeTemp);
                 console.log(dayThreeHumid);
                 console.log(dayThreeIcon);
+                console.log(dayFourDate);
                 console.log(dayFourTemp);
                 console.log(dayFourHumid);
                 console.log(dayFourIcon);
+                console.log(dayFiveDate);
                 console.log(dayFiveTemp);
                 console.log(dayFiveHumid);
                 console.log(dayFiveIcon);
